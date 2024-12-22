b8abdf79acb980c032b936942c12cf74bcd1bb35
Fatme <fatme34saeed@gmail.com>
2024-12-22 17:53:33 +0200

    Step 2 - Create a simple express server

--- server.js
+++ server.js
@@ -11,3 +11,8 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     res.send('ok');
 });
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
